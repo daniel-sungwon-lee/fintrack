5b31187820f9f2ddcb118128928941ec2c34ca82
Daniel Sungwon Lee <danlee333@gmail.com>
2021-08-03 21:27:52 -0700

    updated meta data

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
 import '../styles/globals.css'
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>FinTrack</title>
+        <meta name="description" content="Finance Tracker" />
+        <link rel="icon" href="/money.svg" />
+      </Head>
+      <Component {...pageProps} />
+    </> 
+  )
 }
 
 export default MyApp
