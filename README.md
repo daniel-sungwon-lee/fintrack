4204e0791e839741588e371affd1af934393d53e
Daniel Sungwon Lee <danlee333@gmail.com>
2022-11-08 10:15:16 -0800

    server build

---
+++ README.md
@@ -0,0 +1,2 @@
+# FinTrack
+The finance tracking app
---
+++ server.js
@@ -0,0 +1,110 @@
+require('dotenv/config')
+import { Pool } from 'pg'
+const express = require('express');
+const port = process.env.PORT || 3001;
+const path = require('path');
+const argon2 = require('argon2');
+const jwt = require('jsonwebtoken');
+
+const db = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+const app = express();
+
+//static-middleware
+const publicPath = path.join(__dirname, 'public');
+const staticMiddleware = express.static(publicPath);
+
+app.use(staticMiddleware)
+
+app.use(express.json())
+
+//client-error
+class ClientError {
+  constructor(status, message) {
+    this.status = status;
+    this.message = message;
+  }
+}
+
+//error-middleware
+function errorMiddleware(err, req, res, next) {
+  if (err instanceof ClientError) {
+    res.status(err.status).json({
+      error: err.message
+    });
+  } else {
+    console.error(err);
+    res.status(500).json({
+      error: 'an unexpected error occurred'
+    });
+  }
+}
+
+//API endpoints
+
+//auth
+app.post('/api/signup', (req, res, next) => {
+  const { email, password } = req.body;
+
+  argon2
+    .hash(password)
+    .then(hashedPassword => {
+      const sql = `
+     insert into "users" ("email", "hashedPassword")
+     values ($1, $2)
+     `;
+      const params = [email, hashedPassword];
+
+      db.query(sql, params)
+        .then(result => {
+          res.status(201).json(result.rows[0]);
+        })
+        .catch(err => next(err));
+    })
+    .catch(err => next(err));
+});
+
+app.post('/api/login', (req, res, next) => {
+  const { email, password } = req.body;
+
+  const sql = `
+  select "userId", "hashedPassword", "email"
+  from "users"
+  where "email" = $1
+  `;
+  const params = [email];
+
+  db.query(sql, params)
+    .then(result => {
+      const [user] = result.rows;
+      if (!user) {
+        throw new ClientError(401, 'Invalid login');
+      }
+      const { userId, hashedPassword, email } = user;
+      argon2
+        .verify(hashedPassword, password)
+        .then(isMatch => {
+          if (!isMatch) {
+            throw new ClientError(401, 'Invalid login');
+          }
+          const payload = { userId, email };
+          const token = jwt.sign(payload, process.env.TOKEN_SECRET);
+          res.json({ token, user: payload });
+        })
+        .catch(err => next(err));
+    })
+    .catch(err => next(err));
+});
+
+
+
+app.use(errorMiddleware);
+
+app.listen(port, () => {
+  console.log(`express server listening on ${port}`)
+})
