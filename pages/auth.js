dbf6b533dc71fddb32d025125d752db5c8eb7867
Daniel Sungwon Lee <danlee333@gmail.com>
2022-11-07 18:56:52 -0800

    adding fonts WIP

--- pages/auth.js
+++ pages/auth.js
@@ -1,4 +1,5 @@
 import { Button, TextField } from "@mui/material"
+import Image from "next/dist/client/image"
 
 export default function Auth () {
 
@@ -10,10 +11,14 @@ export default function Auth () {
     <>
       <div className="container">
 
-        <form className="d-flex flex-column align-items-center" onSubmit={handleLogin}>
-          <TextField id="email" required variant="standard" label="Email"></TextField>
-          <TextField id="password" required variant="standard" label="Password"></TextField>
-          <Button variant="contained" style={{color: '#00C169'}}>Login</Button>
+      <div className="d-flex justify-content-center m-5">
+        <Image src="/images/money.svg" alt="FinTrack Logo" width='25%' />
+      </div>
+
+        <form className="d-flex flex-column align-items-center m-5" onSubmit={handleLogin}>
+          <TextField className="mb-3" id="email" required variant="standard" label="Email" InputLabelProps={{required: false}}></TextField>
+          <TextField className="mb-5" id="password" required variant="standard" label="Password" InputLabelProps={{required: false}}></TextField>
+          <Button type="submit" className="mb-5" variant="contained" sx={{backgroundColor: "#00C169"}}>Login</Button>
         </form>
 
       </div>
