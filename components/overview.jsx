0eae90f85a4a1f304264593ac3f37a7cb387b7ef
Daniel Sungwon Lee <danlee333@gmail.com>
2023-04-13 21:27:17 -0700

    account overview WIP

--- components/overview.jsx
+++ components/overview.jsx
@@ -1,4 +1,5 @@
-import { Zoom } from "@mui/material"
+import { Avatar, Card, CardContent, CardHeader, Paper, Zoom } from "@mui/material"
+import { AccountBalanceRounded } from "@mui/icons-material"
 import { useEffect, useState } from "react"
 import Placeholder from "./placeholder"
 
@@ -6,6 +7,7 @@ import Link from './plaid/link.tsx'
 
 export default function Overview() {
   const [loading, setLoading] = useState(true)
+  const [data, setData] = useState(null)
 
   useEffect(() => {
     setLoading(false)
@@ -18,14 +20,65 @@ export default function Overview() {
           ? <Placeholder />
           :
             <Zoom in>
-              <div className="d-flex justify-content-center align-items-center"
+              <div className="d-flex flex-column justify-content-center align-items-center"
                style={{minHeight: '50vh'}}>
 
                 <Link />
 
+                <Account data={data} />
+
               </div>
             </Zoom>
       }
+    </>
+  )
+}
+
+function Account({ data }) {
+  const [loading, setLoading] = useState(true)
+  const [hover, setHover] = useState(false)
+
+  return (
+    <>
+      <Zoom in>
+        <Paper sx={{minWidth: "80%", margin:"5rem 1rem"}} elevation={3}>
+
+          <Card sx={{margin: "3rem"}} raised={hover} onMouseEnter={() => setHover(true)}
+           onMouseLeave={() => setHover(false)}>
+            <CardHeader avatar={
+              <Avatar>
+                <AccountBalanceRounded />
+              </Avatar>
+            } title="Bank Account" />
+            <CardContent>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam quisquam eligendi repellendus voluptas ducimus minus provident rem beatae, quia cumque optio quidem facilis magni quo tenetur! Iste hic alias provident.
+            </CardContent>
+          </Card>
+
+          <Card sx={{ margin: "3rem" }} raised>
+            <CardHeader avatar={
+              <Avatar>
+                <AccountBalanceRounded />
+              </Avatar>
+            } title="Bank Account" />
+            <CardContent>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam quisquam eligendi repellendus voluptas ducimus minus provident rem beatae, quia cumque optio quidem facilis magni quo tenetur! Iste hic alias provident.
+            </CardContent>
+          </Card>
+
+          <Card sx={{ margin: "3rem" }} raised>
+            <CardHeader avatar={
+              <Avatar>
+                <AccountBalanceRounded />
+              </Avatar>
+            } title="Bank Account" />
+            <CardContent>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam quisquam eligendi repellendus voluptas ducimus minus provident rem beatae, quia cumque optio quidem facilis magni quo tenetur! Iste hic alias provident.
+            </CardContent>
+          </Card>
+
+        </Paper>
+      </Zoom>
     </>
   )
 }
