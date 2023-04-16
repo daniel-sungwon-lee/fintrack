7b24281d98f4b18cce7453def96eb8fa702d7ab6
Daniel Sungwon Lee <danlee333@gmail.com>
2023-04-15 21:24:50 -0700

    added skeleton for account card

--- components/overview.jsx
+++ components/overview.jsx
@@ -1,5 +1,5 @@
 import { Avatar, Card, CardContent, CardHeader, Dialog, DialogActions, DialogContent,
-         DialogContentText, DialogTitle, Fab, Slide, Paper, Zoom } from "@mui/material"
+         DialogContentText, DialogTitle, Fab, Slide, Paper, Zoom, Skeleton } from "@mui/material"
 import { AccountBalanceRounded, CloseRounded } from "@mui/icons-material"
 import { useEffect, useState, forwardRef } from "react"
 import Placeholder from "./placeholder"
@@ -41,25 +41,46 @@ function Accounts({ data }) {
   const [loading, setLoading] = useState(true)
   const [open, setOpen] = useState(false)
 
+  useEffect(() => {
+    setLoading(false)
+  })
+
   return (
     <>
       <Zoom in>
         <Paper sx={{minWidth: "80%", margin:"5rem 1rem", bgcolor:"#FFD800"}} elevation={3}>
 
-          <Card sx={{margin: "3rem", cursor: "pointer", borderRadius:"1rem"}} onMouseEnter={(e) =>
-           e.currentTarget.style.boxShadow="0px 5px 5px -3px rgba(0,0,0,0.2), 0px 8px 10px 1px rgba(0,0,0,0.14), 0px 3px 14px 2px rgba(0,0,0,0.12)"}
-           onMouseLeave={(e) => e.currentTarget.style.boxShadow="0px 2px 1px -1px rgba(0,0,0,0.2), 0px 1px 1px 0px rgba(0,0,0,0.14), 0px 1px 3px 0px rgba(0,0,0,0.12)"}
-           onClick={() => setOpen(true)}>
-            <CardHeader avatar={
-              <Avatar sx={{bgcolor:"#FFD800"}}>
-                <AccountBalanceRounded color="primary" />
-              </Avatar>
-            } title="Bank Account" />
-            <CardContent>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam quisquam eligendi repellendus voluptas ducimus minus provident rem beatae, quia cumque optio quidem facilis magni quo tenetur! Iste hic alias provident.
-            </CardContent>
-          </Card>
-
+          { loading
+              ? <Skeleton variant="rectangle" sx={{borderRadius: '1rem', margin: '3rem'}}>
+                  <Card sx={{margin: "", cursor: "pointer", borderRadius:"1rem"}} onMouseEnter={(e) =>
+                   e.currentTarget.style.boxShadow="0px 5px 5px -3px rgba(0,0,0,0.2), 0px 8px 10px 1px rgba(0,0,0,0.14), 0px 3px 14px 2px rgba(0,0,0,0.12)"}
+                   onMouseLeave={(e) => e.currentTarget.style.boxShadow="0px 2px 1px -1px rgba(0,0,0,0.2), 0px 1px 1px 0px rgba(0,0,0,0.14), 0px 1px 3px 0px rgba(0,0,0,0.12)"}
+                   onClick={() => setOpen(true)}>
+                    <CardHeader avatar={
+                      <Avatar sx={{bgcolor:"#FFD800"}}>
+                        <AccountBalanceRounded color="primary" />
+                      </Avatar>
+                    } title="Bank Account" />
+                    <CardContent>
+                      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam quisquam eligendi repellendus voluptas ducimus minus provident rem beatae, quia cumque optio quidem facilis magni quo tenetur! Iste hic alias provident.
+                    </CardContent>
+                  </Card>
+                </Skeleton>
+              :
+                <Card sx={{margin: "3rem", cursor: "pointer", borderRadius:"1rem"}} onMouseEnter={(e) =>
+                e.currentTarget.style.boxShadow="0px 5px 5px -3px rgba(0,0,0,0.2), 0px 8px 10px 1px rgba(0,0,0,0.14), 0px 3px 14px 2px rgba(0,0,0,0.12)"}
+                onMouseLeave={(e) => e.currentTarget.style.boxShadow="0px 2px 1px -1px rgba(0,0,0,0.2), 0px 1px 1px 0px rgba(0,0,0,0.14), 0px 1px 3px 0px rgba(0,0,0,0.12)"}
+                onClick={() => setOpen(true)}>
+                  <CardHeader avatar={
+                    <Avatar sx={{bgcolor:"#FFD800"}}>
+                      <AccountBalanceRounded color="primary" />
+                    </Avatar>
+                  } title="Bank Account" />
+                  <CardContent>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam quisquam eligendi repellendus voluptas ducimus minus provident rem beatae, quia cumque optio quidem facilis magni quo tenetur! Iste hic alias provident.
+                  </CardContent>
+                </Card>
+          }
           <AccountDetails open={open} setOpen={setOpen} data={data} />
 
         </Paper>
